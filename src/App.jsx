3d4d4b712de23c49a3f5cf60d18e9d43caaac13d
Nighter010 <nice.pororock@gmail.com>
2024-02-01 15:08:32 +0700

    1/2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import "./App.css";
 import ItemsList from "./component/Items";
 import Students from "./component/Students";
@@ -6,32 +7,59 @@ import Subjects from "./component/Subjects";
 import ItemFormFind from "./component/itemFormfile";
 import ItemFormFindStu from "./component/studentFormfile";
 import ItemFormFindSub from "./component/subjectFormfile";
+import SignIn from './component/signIn';
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="app-container">
-      <h1 className="app-header">My Front-End</h1>
+     <Router>
+      <div>
+        <h3>My Project</h3>
+        <nav>
+            <Link to="/">หน้าหลัก</Link>
+            <hr />
+            {!isLoggedIn && (
+                <Link to="/signin">เข้าสู่ระบบ</Link>
+            )}
+        </nav>
 
-      <div className="card-container">
-        <div className="card">
-          <h2>Items List</h2>
-          <ItemsList />
-          <ItemFormFind />
-        </div>
-
-        <div className="card">
-          <h2>Subjects</h2>
-          <Subjects />
-          <ItemFormFindSub />
-        </div>
-
-        <div className="card">
-          <h2>Students</h2>
-          <Students />
-          <ItemFormFindStu />
-        </div>
+        {/* Add the Routes component to the App */}
+        {/* <> xxx </>คือตำแหน่งที่ข้อมูลจาก Component คือค่ามา */}
+        <Routes>
+          <Route path="/signin" element={<SignIn onLoginSuccess={handleLoginSuccess} />} />
+          <Route path="/" element={
+            <> 
+              <div className="card">
+                {isLoggedIn ? (
+                  <>
+                    <p>ยินดีต้อนรับ</p>
+                    <p><ItemsList/></p>
+                    <hr/>
+                    <p><Students/></p>
+                    <hr/>
+                    <p><Subjects/></p>
+                    <hr/>
+                    <p><ItemFormFind/></p>
+                    <hr/>
+                    <p><ItemFormFindStu/></p>
+                    <hr/>
+                    <p><ItemFormFindSub/></p>
+                  </>
+                ) : (
+                  <p>กรุณา Login </p>
+                )}
+              </div>
+            </>
+          }/>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
